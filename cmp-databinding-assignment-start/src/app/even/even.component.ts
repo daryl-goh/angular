2c76631d93fc87775aa68187e90f5d6995a0f68b
daryl-goh <goh.daryl@gmail.com>
2023-01-13 18:20:59 +0800

    Assignment 4: Practicing Property & Event Binding and View Encapsulation

---
+++ cmp-databinding-assignment-start/src/app/even/even.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-even',
+  templateUrl: './even.component.html',
+  styleUrls: ['./even.component.css']
+})
+export class EvenComponent {
+  @Input() number: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
